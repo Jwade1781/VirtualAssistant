11fbe0df6c5010405f06d9400e9ce294b62dd100
Jwade1781 <46077645+Jwade1781@users.noreply.github.com>
2022-04-13 15:22:24 -0400

    Initial Class layout of SQLite Database
    Initial Class layout of the SQLite databases that will be expanded & utilized later

---
+++ Databases/SQLiteDatabase.py
@@ -0,0 +1,35 @@
+#######################################################################
+#
+# Purpose:
+# The purpose of this program is to easily handle SQLite databases within the project
+#
+# Extra Info:
+# --
+#
+# Run:
+# --
+#
+#######################################################################
+
+class SQLiteDatabase:
+    import sqlite3
+    def __init__(self):
+        connection = None
+      
+    def _OpenDatabase(self):
+        import OpenSQLiteDatabase as openDB
+        return
+    
+    def _CreateDatabase(self):
+        import CreateSQLiteDatabase as createDB
+        return
+    
+    def _BackupDatabase(self):
+        import BackupSQLiteDatabase as backupDB
+        return
+    
+    def _DumpDatabase(self):
+        import DumpSQLiteDatabase as dumpDB
+        return
+    
+    
