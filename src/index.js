13aebbd01af7d466ff6a6616671f953d4b01cb0b
Jwade1781 <46077645+Jwade1781@users.noreply.github.com>
2022-04-20 21:14:36 -0400

    Updated Starting UI with BootStrap
    Updated Starting UI with BootStrap and Vanta to be more visually appealing, starting to implement NodeJS's child shell processes to execute python code. This will be used to start the backend and allow for communication.

--- src/index.js
+++ src/index.js
@@ -43,5 +43,12 @@ app.on('activate', () => {
   }
 });
 
+function StartPythonServer(){
+
+  return "Hello World";
+}
+
+StartPythonServer();
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
